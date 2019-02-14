1fcebed4647448862f0a53c4e6750a89213dfde4
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-14 14:16:34 -0500

    update text to inherited stock config setings

--- models/inherited_stock_config_settings.py
+++ models/inherited_stock_config_settings.py
@@ -9,8 +9,8 @@ class ResConfigSettings(models.TransientModel):
 	_inherit = ['res.config.settings']
 
 
-	notification_base = fields.Selection([('on_hand','Cantidades a la mano'),('fore_cast','')],related='company_id.notification_base',string='Notification Based On')
-	notification_products = fields.Selection([('for_all','Global para todos los productos'),('fore_product',' Individial para todos los productos')],related='company_id.notification_products',string='Min Quantity Based On')
+	notification_base = fields.Selection([('on_hand','Cantidades a la mano'),('fore_cast','')],related='company_id.notification_base',string='Notificacion basada en: ')
+	notification_products = fields.Selection([('for_all','Global para todos los productos'),('fore_product',' Individial para todos los productos')],related='company_id.notification_products',string='Cantidad minima basada en :')
 	min_quantity = fields.Float(string = 'Cantidad limite',related='company_id.min_quantity')
 	notification_user_id = fields.Many2one('res.users',related='company_id.notification_user_id',string = 'Notify User')
 	email_user = fields.Char( string="Email From",related='company_id.email')
