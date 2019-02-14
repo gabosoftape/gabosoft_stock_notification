a874f841e2d2c117e30b8ff5aefdee6cf22963f6
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-14 15:19:33 -0500

    update syntax and write code in spanish

--- models/inherited_res_company.py
+++ models/inherited_res_company.py
@@ -9,7 +9,7 @@ class Company(models.Model):
 
 
 
-	notification_base = fields.Selection([('on_hand','On hand quantity'),('fore_cast','Forecast')],string='Notification Based On',default='on_hand')
-	notification_products = fields.Selection([('for_all','Global for all product'),('fore_product',' Individual for all products')],string='Min Quantity Based On',default='for_all')
-	min_quantity = fields.Float(string = 'Quantity Limit')
-	notification_user_id = fields.Many2one('res.users',string = 'Notify User')
+	notification_base = fields.Selection([('on_hand','Cantidad a mano'),('fore_cast','Prevista')],string='Notificacion basada en: ',default='on_hand')
+	notification_products = fields.Selection([('for_all','Global para todos los productos'),('fore_product',' Individual para todos los productos ')],string='Cantidad minima basada en',default='for_all')
+	min_quantity = fields.Float(string = 'Cantidad limite')
+	notification_user_id = fields.Many2one('res.users',string = 'Usuario para Notificacion')
