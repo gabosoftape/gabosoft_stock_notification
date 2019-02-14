e649fe45452d4c9600f567320efb60b96a0a3c88
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-14 14:07:27 -0500

    update test form spanish

--- models/inherited_stock_config_settings.py
+++ models/inherited_stock_config_settings.py
@@ -9,9 +9,9 @@ class ResConfigSettings(models.TransientModel):
 	_inherit = ['res.config.settings']
 
 
-	notification_base = fields.Selection([('on_hand','On hand quantity'),('fore_cast','Forecast')],related='company_id.notification_base',string='Notification Based On')
-	notification_products = fields.Selection([('for_all','Global for all product'),('fore_product',' Individual for all products')],related='company_id.notification_products',string='Min Quantity Based On')
-	min_quantity = fields.Float(string = 'Quantity Limit',related='company_id.min_quantity')
+	notification_base = fields.Selection([('on_hand','Cantidades a la mano'),('fore_cast','Forecast')],related='company_id.notification_base',string='Notification Based On')
+	notification_products = fields.Selection([('for_all','Global para todos los productos'),('fore_product',' Individual for all products')],related='company_id.notification_products',string='Min Quantity Based On')
+	min_quantity = fields.Float(string = 'Cantidad limite',related='company_id.min_quantity')
 	notification_user_id = fields.Many2one('res.users',related='company_id.notification_user_id',string = 'Notify User')
 	email_user = fields.Char( string="Email From",related='company_id.email') 
 	low_stock_products_ids = fields.One2many('low.stock.transient','stock_product_id',store=True)
@@ -20,30 +20,30 @@ class ResConfigSettings(models.TransientModel):
 
 	def action_list_products_(self):
 		products_list=[]
-		
+
 		res = self.env['res.config.settings'].search([],order="id desc", limit=1)
 		if res.id :
 			products_dlt = [(2,dlt.id,0)for dlt in res.low_stock_products_ids]
-			
-		
+
+
 			res.low_stock_products_ids = products_dlt
 
 			if res.notification_base == 'on_hand':
 				if res.notification_products == 'for_all':
 					result = self.env['product.product'].search([('qty_available','<',res.min_quantity)])
-					
-					
+
+
 					for product in result:
-						
+
 						products_list.append([0,0,{'name':product.name,
 												'limit_quantity':res.min_quantity,
 												'stock_quantity':product.qty_available}])
-		
-				
+
+
 				if res.notification_products == 'fore_product':
-					
+
 					result = self.env['product.product'].search([])
-					
+
 
 					for product in result:
 						if product.qty_available < product.min_quantity:
@@ -67,41 +67,41 @@ class ResConfigSettings(models.TransientModel):
 														'limit_quantity':product.min_quantity,
 													'stock_quantity':product.virtual_available}])
 
-				
 
-			
+
+
 			res.low_stock_products_ids = products_list
 
-				
-			return 
+
+			return
 		else :
 			return
 
 
 
 
-	
-	
-		
+
+
+
 	def action_low_stock_send(self):
 		self.action_list_products_()
-		
+
 		res = self.env['res.config.settings'].search([],order="id desc", limit=1)
-		
+
 		if res.id :
-		
-		
-			
+
+
+
 			template_id = self.env.ref('bi_product_low_stock_notification.low_stock_email_template')
-			
+
 			send = template_id.send_mail(res.id, force_send=True)
-		
+
 			return True
 
-		
-		
+
+
 		return True
-	
+
 
 
 class low_stock_product(models.TransientModel):
@@ -112,4 +112,3 @@ class low_stock_product(models.TransientModel):
 	stock_quantity=fields.Float(string='Quantity')
 	limit_quantity=fields.Float(string='Quantity limit')
 	stock_product_id=fields.Many2one('res.config.settings')
-
